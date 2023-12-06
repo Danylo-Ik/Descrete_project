a923ebad59ed1e43ad6b3952e8e3440d673b48f2
Korch195 <korchevyi.pn@ucu.edu.ua>
2023-12-06 15:57:23 +0200

    Перша версія, ліст компрехеншн

--- pathfinding.py
+++ pathfinding.py
@@ -1,4 +1,5 @@
-def read_graph(pathname: str) -> List(List(int))):
+from typing import List
+def read_graph(pathname: str) -> List[List[int]]:
 	"""
 	read graph from csv file and save it is a list of lists
 	[[1,2,3,4,5],
@@ -7,16 +8,19 @@ def read_graph(pathname: str) -> List(List(int))):
 	"""
 	pass
 
-def generate_graph(width: int, length: int) -> List(List(int))):
+def generate_graph(width: int, length: int) -> List[List[int]]:
 	"""
  	generate a graph with given length and size and fill it with random heights
   	width - number of int's in every list
    	length - number of lists inside main list
   	"""
-	pass
+	import random
+	graph_list = [[random.randrange(1, 10) for k in range(width)] for i in range(length)]
+	return graph_list
+print(generate_graph(7, 5))
 	
 
-def find_shortest_path(graph: List(List(int)), step: int, start: tuple(int, int), end: tuple(int, int) -> List(tuple(int, int):
+def find_shortest_path(graph, step, start, end):
 	"""
  	find the shortest path between two points and return list of pairs of indexes
   	"""
