719bbddc04c3d68765ba5cdf0fe57de58bfae57c
Danyil Ikonnikov <149335118+Danylo-Ik@users.noreply.github.com>
2023-12-06 14:11:42 +0200

    Create pathfinding.py

---
+++ pathfinding.py
@@ -0,0 +1,22 @@
+def read_graph(pathname: str) -> List(List(int))):
+	"""
+	read graph from csv file and save it is a list of lists
+	[[1,2,3,4,5],
+	[6,7,8,9,10],
+	[11,12,13,14,15]]
+	"""
+	pass
+
+def generate_graph(width: int, length: int) -> List(List(int))):
+	"""
+ 	generate a graph with given length and size and fill it with random heights
+  	"""
+	pass
+	
+
+def find_shortest_path(graph: List(List(int)), step: int, start: tuple(int, int), end: tuple(int, int) -> List(tuple(int, int):
+	"""
+ 	find the shortest path between two points and return list of pairs of indexes
+  	"""
+	pass
+	
